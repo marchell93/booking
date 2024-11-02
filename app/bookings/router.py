75e88ce73f7f99c2fa8df40a9824d65ddc01e47e
Mark P. <marchell93@mail.ru>
2024-11-02 12:12:05 +0300

    Описали схему для Booking

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBooking
 
 router = APIRouter(
     prefix="/bookings",
@@ -8,5 +9,5 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings():
-    return await BookingDAO.find_one_or_none(room_id=1)
+async def get_bookings() -> list[SBooking]:
+    return await BookingDAO.find_one_or_none()
